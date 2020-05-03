9a69ba5730e9e9299d516e26327cb8ef1cf26e6a
riku0389 <riku0389@gmail.com>
2020-05-03 15:35:37 +0800

    add Return.java，OpBitEquals.java

---
+++ src/com/javaExample/Ternary.java
@@ -0,0 +1,11 @@
+package com.javaExample;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absilute value of ");
+        System.out.println(i + " is " + k);
+    }
+}
